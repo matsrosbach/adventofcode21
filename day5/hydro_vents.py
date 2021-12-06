353f908db8764950def33424ef63f2315b097708
Mats Rosbach <matsrosbach@gmail.com>
2021-12-06 08:17:41 +0100

    Added (close to a) solution to day 5, part 1

---
+++ day5/hydro_vents.py
@@ -0,0 +1,54 @@
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def input_to_line(row):
+    p1 = Point(int(row.split(" -> ")[0].split(",")[0]),int(row.split(" -> ")[0].split(",")[1]))
+    p2 = Point(int(row.split(" -> ")[1].split(",")[0]),int(row.split(" -> ")[1].split(",")[1]))
+
+    return Line(p1, p2)
+
+def get_all_lines(input):
+    lines = []
+    for row in input:
+        lines.append(input_to_line(row))
+    return lines
+
+def create_map(lines):
+    map = [[0 for c in range(10)] for r in range(10)]
+    for line in lines:
+        if (line.p1.x == line.p2.x):
+            for yIndex in range(line.p1.y, line.p2.y):
+                map[line.p1.x][yIndex] += 1
+        elif (line.p1.y == line.p2.y):
+            for xIndex in range(line.p1.x, line.p2.x):
+                map[xIndex][line.p1.y] += 1
+    return map
+
+inputFile = open("small_input5", "r")
+
+input = inputFile.read().splitlines()
+
+lines = get_all_lines(input)
+
+map = create_map(lines)
+
+for row in map:
+    print(row)
+
+counter = 0
+for row in map:
+    for element in row:
+        if element > 1:
+            counter += 1
+
+print('The answer is: ', counter)
+
+
