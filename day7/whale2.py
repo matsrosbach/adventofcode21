d8057a435f0ade6a9afed16dbad244960506c1d7
Mats Rosbach <matsholm.rosbach@drift.oslo.kommune.no>
2021-12-07 07:59:39 +0100

    Added solution for day 7, both parts

---
+++ day7/whale2.py
@@ -0,0 +1,19 @@
+inputFile = open("input7", "r")
+
+input = [int(i) for i in inputFile.read().split(",")]
+
+lowestIdx = None
+lowestSum = None
+currentSum = 0
+for idx in range(0, len(input)):
+    for element in input:
+        currentSum += abs(element - idx) * (abs(element-idx)+1) / 2
+
+    if (lowestSum == None or lowestSum > currentSum):
+        lowestSum = currentSum
+        lowestIdx = idx
+        currentSum = 0
+
+print('The answer is: ', lowestSum)
+
+
